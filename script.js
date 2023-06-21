6f41238d90c0bd5b64695b9b9eeee06ecc1b5bf2
luizh3nrique <luizh3nrique00@gmail.com>
2023-06-21 10:24:27 -0300

    Signed-off-by: luizh3nrique <luizh3nrique00@gmail.com>

---
+++ script.js
@@ -0,0 +1,52 @@
+const soma = document.querySelector('#soma');
+const subtracao = document.querySelector('#subtracao');
+const vezes = document.querySelector('#vezes');
+const divisao = document.querySelector('#divisao');
+const resposta = document.querySelector('.resposta');
+const clear = document.querySelector('.clear');
+
+
+soma.addEventListener('click', () => {
+    const input1 = document.querySelector('#namber1').value;
+    const input2 = document.querySelector('#namber2').value;
+
+    const result = Number(input1) + Number(input2);
+
+    resposta.innerHTML = result;
+
+
+});
+
+subtracao.addEventListener('click', () => {
+    const input1 = document.querySelector('#namber1').value;
+    const input2 = document.querySelector('#namber2').value;
+
+    const result = Number(input1) - Number(input2);
+
+    resposta.innerHTML = result;
+});
+
+vezes.addEventListener('click', () => {
+    const input1 = document.querySelector('#namber1').value;
+    const input2 = document.querySelector('#namber2').value;
+
+    const result = Number(input1) * Number(input2);
+
+    resposta.innerHTML = result;
+});
+
+divisao.addEventListener('click', () => {
+    const input1 = document.querySelector('#namber1').value;
+    const input2 = document.querySelector('#namber2').value;
+
+    const result = Number(input1) / Number(input2);
+
+    resposta.innerHTML = result;
+});
+
+clear.addEventListener('click', () => {
+    const input1 = document.querySelector('#namber1').value = '';
+    const input2 = document.querySelector('#namber2').value = '';
+    resposta.innerHTML = '';
+
+});
